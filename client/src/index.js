1b7fa3a70f9bd6eac5c41198f37100cd644f29d9
Miguel Angel Guante Suero <49294128+AngelGuante@users.noreply.github.com>
2023-07-21 16:40:09 +0000

    Adding client item UI

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import Stripe_Prebuild_Payment from './componets/Stripe_Prebuild_Payment'
+
+function App() {
+    return (
+        <Stripe_Prebuild_Payment />
+    )
+}
+
+const root = createRoot(document.getElementById('root'))
+root.render(<App />, root)
