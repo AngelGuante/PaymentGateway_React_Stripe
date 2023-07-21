87e093fa83c65b82621d3368c48ed01a53cccfca
Miguel Angel Guante Suero <49294128+AngelGuante@users.noreply.github.com>
2023-07-21 20:10:51 +0200

    Update InstallDependencies.js

--- scripts/InstallDependencies.js
+++ scripts/InstallDependencies.js
@@ -1,3 +1,4 @@
+//This script is to install dependencies into the server and the client at the same time
 const child_process = require('child_process')
 
 const installServerDependencies = () => child_process.exec('npm install --prefix server')
